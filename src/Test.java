443f85a9c2e9e6768567be9b807ef0605896c9d3
rovshen89 <rovshen89@gmail.com>
2020-01-31 17:57:16 -0600

    very first commit

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
+        int x = 1;
+        int y = 2;
+        System.out.println((x)+y);
     }
 }
