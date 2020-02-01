099e0351139956bb3701ba6a1320e58138529d10
rovshen89 <rovshen89@gmail.com>
2020-02-01 16:55:25 -0600

    first day commit

--- src/firstpackage/FirsClass.java
+++ src/firstpackage/FirsClass.java
@@ -2,6 +2,19 @@ package firstpackage;
 
 public class FirsClass {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        /*int l = 14;
+        int bb = 3;
+        System.out.println("Perimeter of rectangle: " + 2 * (l + bb));
+
+        int celciusDegree = 40;
+        System.out.println("Fahrenheit: " + (celciusDegree * 9 / 5 + 32));*/
+
+        String newString = "Java is fun?";
+        float newFloat = 78.98f;
+        double newDouble = 78.980000;
+        System.out.format("Do you think %s \n", newString);
+        System.out.format("\tYes %%%f of the people think it is fun\n", newDouble);
+        System.out.format("\tYes %%%.2f of the people thinks is is fun", newFloat);
+
+        }
 }
