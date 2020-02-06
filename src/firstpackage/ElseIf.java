172801c6fa475cf0c2e1e3f3f7c0d8381865795b
rovshen89 <rovshen89@gmail.com>
2020-02-06 16:56:49 -0600

    java 2/6/20

---
+++ src/firstpackage/ElseIf.java
@@ -0,0 +1,24 @@
+package firstpackage;
+
+import java.util.Scanner;
+
+public class ElseIf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        if(N % 2 == 1){
+            System.out.println("odd");
+        }
+        else if(N % 2 == 0 && (N > 2 && N < 6)){
+            System.out.println("Not weird");
+        }
+        else if(N % 2 == 0 && (N > 6 && N < 20)){
+            System.out.println("Weird");
+        }
+        else if(N % 2 == 0 && N > 20){
+            System.out.println("Not Weird");
+        }
+
+    }
+}
