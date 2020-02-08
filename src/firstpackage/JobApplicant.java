78011adf64ed188d7207375a710387c51688580c
rovshen89 <rovshen89@gmail.com>
2020-02-08 12:50:48 -0600

    java 2/8/20

--- src/firstpackage/JobApplicant.java
+++ src/firstpackage/JobApplicant.java
@@ -13,17 +13,22 @@ public class JobApplicant {
 
         if(age > 20 && age < 30 && javaPoint >= 80){
            score = ((age * 1.5) + (javaPoint * 4.5));
+            System.out.println("Your app score: " + score);
         }
         else if(age > 20 && age < 30 && javaPoint <= 80){
             score = ((age * 1.5) + (javaPoint * 3.5));
+            System.out.println("Your app score: " + score);
         }
         else if (age > 31 && javaPoint > 80){
             score = ((age * 1.1) + (age * 4.5));
+            System.out.println("Your app score: " + score);
         }
         else if (age > 31 && javaPoint <= 80){
             score = ((age * 1.5) + (javaPoint * 3.5));
             System.out.println("Your app score: " + score);
         }
-
+        else{
+            System.out.println("Please enter valid age or JavaPoint");
+        }
     }
 }
