5de614231c75b066240656258ddb67d88ee84de6
rovshen89 <rovshen89@gmail.com>
2020-02-11 17:50:27 -0600

    java 2/10/20

---
+++ src/firstpackage/TernerOperator.java
@@ -0,0 +1,8 @@
+package firstpackage;
+
+public class TernerOperator {
+    public static void main(String[] args) {
+        String answer = (1 == 1)? "True" : "False";
+        System.out.println(answer);
+    }
+}
