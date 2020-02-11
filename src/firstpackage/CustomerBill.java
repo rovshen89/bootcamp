0fbdb9357e7251795d4116e45a2516b88428b980
rovshen89 <rovshen89@gmail.com>
2020-02-11 14:22:15 -0600

    java 2/10/20

--- src/firstpackage/CustomerBill.java
+++ src/firstpackage/CustomerBill.java
@@ -6,6 +6,23 @@ public class CustomerBill {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Do you have subscription (Y/N)? : ");
-        
+        String subscription = scanner.next();
+
+        if(subscription.equals("Y")){
+            System.out.println("Average usage GB: ");
+            int usage = scanner.nextInt();
+
+            if(usage <= 100){
+                System.out.println("Monthly payment will be $10");
+            }
+            else if(usage >= 101){
+                System.out.println("Monthly payment will be $40");
+            }
+        }
+        else{
+            System.out.println("Please visit subscription page");
+        }
+
+
     }
 }
--- src/firstpackage/NestedIf.java
+++ src/firstpackage/NestedIf.java
@@ -5,7 +5,7 @@ public class NestedIf {
         int x = 1;
         int y = -4;
 
-        if(x == 1){
+        if(x != 1){         //when condition not meets requirement
             System.out.println("1st message");
             if(y > 0){
                 System.out.println("2nd message?");
