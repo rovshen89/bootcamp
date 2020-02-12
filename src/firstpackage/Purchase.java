881966685e8a156ba6393f1413cfdd8e70e0d032
rovshen89 <rovshen89@gmail.com>
2020-02-12 13:30:48 -0600

    java 2/12/20

---
+++ src/firstpackage/Purchase.java
@@ -0,0 +1,26 @@
+package firstpackage;
+
+import java.util.Scanner;
+
+public class Purchase {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Math or Art?: ");
+        String select = input.next();
+
+        if(select.equalsIgnoreCase("math")){
+            System.out.println("Do you want tutor? Y/N: ");
+            String tutor = input.next();
+            if(tutor.equalsIgnoreCase("y")){
+                System.out.println("With tutor $100.");
+            }
+            else{
+                System.out.println("Study yourself!");
+            }
+
+        }
+        else {
+            System.out.println("Nice choice.");
+        }
+    }
+}
