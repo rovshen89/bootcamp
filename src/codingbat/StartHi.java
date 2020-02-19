b4ccedb845e6e63cf4ee259fdb15f4de9f7d3bba
rovshen89 <rovshen89@gmail.com>
2020-02-19 15:25:37 -0600

    CodingBat 2/19/20

---
+++ src/codingbat/StartHi.java
@@ -0,0 +1,29 @@
+package codingbat;
+
+import java.util.Scanner;
+
+public class StartHi {
+    public static boolean StartHi(String[] args) {
+        //Given a string, return true if the string starts with "hi" and false otherwise.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter string");
+        String str = scanner.next();
+
+        if(str.length() < 3)
+            return false;
+
+        String firstTwo = str.substring(0,2);
+
+        if(firstTwo.equalsIgnoreCase("hi")){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
