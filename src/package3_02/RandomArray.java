b89fc3ae6da26b68f42d7cec8f68ec1396edb85b
rovshen89 <rovshen89@gmail.com>
2020-03-04 17:05:30 -0600

    Bootcamp 3/4/20

--- src/package3_02/RandomArray.java
+++ src/package3_02/RandomArray.java
@@ -5,17 +5,32 @@ import java.util.Arrays;
 public class RandomArray {
     public static void main(String[] args) {
 
-        int [] randomArray = new int[5];
+        /*int[] randomArray = new int[5];
 
-        for (int i = 0; i < randomArray.length; i++){
-            randomArray[i] = (int)(10+(Math.random()*20));;
-            System.out.print(randomArray[i] + " ");
-
-        }
-//        for (int number: randomArray){
-//            System.out.println(number);
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = (int) (10 + (Math.random() * 20));
         }
 
+        for (int number : randomArray) {
+            System.out.println(number);
+        }*/
 
+        String[] arrayString = new String[10];
+        for (int i = 0; i < arrayString.length; i++){
+            arrayString[i] = generateRandWord();
+        }
+
+        for (String word: arrayString){
+            System.out.print(word + " ");
+        }
     }
 
+    public static String generateRandWord(){
+        String word = " ";
+        int wordLength = (int)(Math.random()*6+1);
+        for (int i = 0; i < wordLength;i++){
+            word += (char)(Math.random()*26+97);
+        }
+        return word;
+    }
+}
