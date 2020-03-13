b646407093207e7be0d0eb9f2a9374a38d0a2650
rovshen89 <rovshen89@gmail.com>
2020-03-12 19:14:46 -0500

    Bootcamp 3/9/20

---
+++ src/package_312/DecisionString.java
@@ -0,0 +1,30 @@
+package package_312;
+
+public class DecisionString {
+    public static void main(String[] args) {
+        decideString("It Is Time to Review java");
+
+
+    }
+
+    public static int countAllCapitals(String sentence){
+        int numberOfCapitals = 0;
+        for (int i = 0; i < sentence.length(); i++){
+            if (sentence.charAt(i) > 64 && sentence.charAt(i) < 92){
+                numberOfCapitals++;
+            }
+        }
+        return numberOfCapitals;
+    }
+
+    public static void decideString(String sentence){
+        int numCapitals = countAllCapitals(sentence);
+        if (numCapitals > 4){
+            System.out.println("Accepted");
+        }else{
+            System.out.println("Rejected");
+        }
+    }
+}
+
+
---
+++ src/package_312/TravelSuggestion.java
@@ -0,0 +1,43 @@
+package package_312;
+
+import java.util.Scanner;
+
+public class TravelSuggestion {
+    public static void main(String[] args) {
+        suggestDestination();
+    }
+
+    public static void suggestDestination(){
+        Scanner myScan = new Scanner(System.in);
+        String destination = "";
+        System.out.println("Sea or Mountain: ");
+        String answer = myScan.nextLine();
+        if (answer.equalsIgnoreCase("sea")){
+            System.out.println("East or West: ");
+            answer = myScan.nextLine();
+            if (answer.equalsIgnoreCase("east")){
+                destination = "Florida";
+            }else if (answer.equalsIgnoreCase("west")){
+                destination = "California";
+            }else{
+                System.out.println("Wrong input");
+            }
+
+        }
+        else if (answer.equalsIgnoreCase("mountain")){
+            System.out.println("Midland or North: ");
+            answer = myScan.nextLine();
+            if (answer.equalsIgnoreCase("midland")){
+                destination = "Kansas";
+            }else if (answer.equalsIgnoreCase("north")){
+                destination = "Vermont";
+            }else{
+                System.out.println("Wrong input");
+            }
+
+        }else{
+            System.out.println("Wrong input");
+        }
+        System.out.println(destination);
+    }
+}
