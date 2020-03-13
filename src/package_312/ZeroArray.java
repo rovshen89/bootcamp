17f979b0981cc3fe2cc18205121b354254974992
rovshen89 <rovshen89@gmail.com>
2020-03-13 18:16:45 -0500

    Bootcamp 3/13/20

---
+++ src/package_312/ZeroArray.java
@@ -0,0 +1,24 @@
+package package_312;
+
+import java.util.Arrays;
+
+public class ZeroArray {
+    public static void main(String[] args) {
+        int[] myArr = {0,9,4,3,0,2,1,0,9,0,0};
+        int[] result = zeroArray(myArr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] zeroArray(int[] array){
+        int[] index = new int[array.length];
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++){
+            if (array[i] != 0){
+                index[count] = array[i];
+                count++;
+            }
+        }
+        return index;
+    }
+}
