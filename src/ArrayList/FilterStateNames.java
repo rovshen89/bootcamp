3978261431d9bd1fdf12272700d4159f6a244c27
rovshen89 <rovshen89@gmail.com>
2020-04-25 12:15:18 -0500

    Bootcamp 4/25/20

--- src/ArrayList/FilterStateNames.java
+++ src/ArrayList/FilterStateNames.java
@@ -1,11 +1,14 @@
 package ArrayList;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class FilterStateNames {
     public static void main(String[] args) {
         String[] names = {"Arizona", "CA", "NV", "Nevada"};
         filterNames(names, "full");
+        System.out.println(filterStates(names, "abb"));
 
     }
     public static void filterNames(String[] name, String param){
@@ -26,5 +29,25 @@ public class FilterStateNames {
         }else{
             System.out.println("Invalid Parameter");
         }
+    }
+
+    public static List<String> filterStates(String[] states, String param){
+        List<String> st = new ArrayList<>();
+        if (param.equals("abb")){
+            for (String state: states){
+                if (state.length() == 2){
+                    st.add(state);
+                }
+            }
+        }else if (param.equals("full")){
+            for (String state: states){
+                if (state.length() > 2){
+                    st.add(state);
+                }
+            }
+        }else {
+            System.out.println("Invalid Parameter");
+        }
+        return st;
     }
 }
